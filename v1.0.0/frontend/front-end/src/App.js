214e84aa5fdb07dcf151eb7712a5e6d81e1423f3
Jaedon Paget <pagetj@tcd.ie>
2024-02-06 14:45:52 +0000

    Added a simple landing page
    The landing page is just simple which we can work with to get more functionality and then adjust the aesthetics of it

--- v1.0.0/frontend/front-end/src/App.js
+++ v1.0.0/frontend/front-end/src/App.js
@@ -1,23 +1,33 @@
+import React from 'react';
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container-fluid d-flex justify-content-center align-items-center vh-100">
+        <div className="cover-container">
+          <header className="masthead mb-auto">
+            <div className="inner">
+              <h3 className="masthead-brand">Group 21 AI Chatbot</h3>
+              <nav className="nav nav-masthead justify-content-center">
+                <a className="nav-link active">Insert Document</a>
+                <a className="nav-link">Chat With Me</a>
+                <a className="nav-link">About Us</a>
+              </nav>
+            </div>
+          </header>
+
+          <main role="main" className="inner cover">
+            <h1 className="cover-heading">Welcome to Group 21's AI chatbot</h1>
+            <p className="lead">Welcome to the Chatbot designed by Group 21 as part of our work with IBM and Large Language Models. Just insert your document and ask as many questions as you want!</p>
+            <p className="lead">
+              <a href="https://elastic.co/what-is/large-language-models" className="btn btn-lg btn-secondary">Learn more</a>
+            </p>
+          </main>
+        </div>
+      </div>
     </div>
   );
 }
