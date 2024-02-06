334439a24c3e38c21a095348a65cd087f5eb7889
Naza <uzoukwuc@tcd.ie>
2024-02-06 02:48:10 +0000

    Add RAG → query generator

--- ChatbotConcept.py
+++ ChatbotConcept.py
@@ -1,72 +1,155 @@
-import streamlit
-
 import os
+import os.path
 
-os.environ["OPENAI_API_KEY"] = ""
-from langchain_openai import ChatOpenAI
-llm = ChatOpenAI(temperature = 0.6)
+# Document loading and the linke
+from langchain_community.document_loaders import TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from sentence_transformers import SentenceTransformer
+from langchain_core.documents.base import Document
+from langchain_community.vectorstores import Chroma
+from langchain_core.runnables import RunnablePassthrough
 
-title = streamlit.title("Q&A Chatbot")
-prompt = streamlit.subheader("Select options in sidebar")
+# ✨AI✨
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate, ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_community.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+from langchain.memory import ChatMessageHistory
+from langchain.chains.combine_documents import create_stuff_documents_chain
 
-language = streamlit.sidebar.selectbox("Select language / Roghnaigh teanga:", ("English", "Gaeilge"))
+# Our own stuff
+from normalise import Datafiniti
+from local import resolve
 
+# streamlit
+import streamlit as st
 
+@st.cache_resource()
+def get_db():
+    documents = Datafiniti("Datafiniti_Amazon_Consumer_Reviews_of_Amazon_Products.csv").load()[:10] # take the first 10 rows
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=250, chunk_overlap=50)
+    split_documents = text_splitter.split_documents(documents)
+    model = SentenceTransformer("all-MiniLM-L6-v2")
+    if os.path.isdir(".chroma_db"):
+        print("Loading chromadb from filesystem")
+        db = Chroma(
+            persist_directory=".chroma_db",
+            embedding_function=SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+        )
+    else:
+        print("Creating new embeddings")
+        db = Chroma.from_documents(
+            split_documents,
+            SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2"),
+            ids=[str(i) for i in range(len(split_documents))],
+            persist_directory=".chroma_db"
+        )
+    return db
 
+@st.cache_resource()
+def get_llm():
+    if os.getenv("LLM") == "LLAMA":
+        from langchain_community.llms import LlamaCpp
+        from langchain.callbacks.manager import CallbackManager
+        from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+        callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+        llm = LlamaCpp(
+            model_path=os.getenv("LLAMA_MODEL_PATH"),
+            callback_manager = callback_manager,
+            verbose = True,
+            n_ctx=1024,
+        )
+    else:
+        from langchain_openai import ChatOpenAI
+        llm = ChatOpenAI(temperature = 0.6)
+        
+    return llm
+
+@st.cache_resource()
+def get_memory():
+    memory = ChatMessageHistory()
+    return memory
+
+@st.cache_resource()
+def get_options():
+    return { "language" : "English" }
+
+def query_chain(retriever):
+    return (lambda params: params["messages"][-1].content) | retriever
+
+db = get_db()
+llm = get_llm()
+memory = get_memory()
+options = get_options()
+
+language = st.sidebar.selectbox(resolve(options["language"], "select_language"), ("English", "Gaeilge"))
+llm_selection = st.sidebar.selectbox(resolve(options["language"], "select_llm"), ("LLaMa", "OpenAI", "Google", "Meta", "IBM"))
+question = st.chat_input(resolve(options["language"], "prompt_placeholder"))
 
 if language:
-    global language_context
-    if language == "English":
-        global llm_selection
-        language_context = ""
-        # print("English selected")
-        title.empty()
-        title = streamlit.title("IBM Q&A Chatbot")
-        prompt.empty()
-        prompt = streamlit.subheader("Select options in sidebar.")
-        llm_selection = streamlit.sidebar.selectbox("Select LLM:", ("","OpenAI", "Google", "Meta", "IBM"))
-    elif language == "Gaeilge":
-        language_context = ". Freagair as Gaeilge. Answer in Irish."
-        # print("Gaeilge roghnaithe")
-        llm_selection = streamlit.sidebar.selectbox("Roghnaigh LLM:", ("","OpenAI", "Google", "Meta", "IBM"))
-        title.empty()
-        title = streamlit.title("Bota Comhrá C⁊F IBM")
-        prompt.empty()
-        prompt = streamlit.subheader("Roghnaigh roghanna ar an dtaobhbharra.")
+    options["language"] = language
 
-def answer_question(question, key):
-    if question:
-        print('Question: "' + question + '"')
-        open_ai_answer = ""
-        with streamlit.spinner('Loading...'):
-            open_ai_answer = llm.invoke(question + language_context).content
-        streamlit.text_area(llm_selection + ":",
-            open_ai_answer,
-            disabled = True,
-            key = "answer " + str(key)
-            )
-        if language == "English":
-            answer_question(streamlit.text_input("You:", placeholder="Write your question...", key = "response " + str(key)), key + 1)
-        elif language == "Gaeilge":
-            answer_question(streamlit.text_input("Tusa:", placeholder="Clóscríobh do theachtaireacht...", key = "response " + str(key)), key + 1)
+st.title(resolve(options["language"], "title"))
 
-if llm_selection:
-    # print(llm_selection + " selected")
-    if llm_selection == "OpenAI":
-        prompt.empty()
-        questions = []
-        if language == "English":
-            question = streamlit.text_input("You:", placeholder="Write your question...")
-            answer_question(question, 1)
-        elif language == "Gaeilge":
-            question = streamlit.text_input("Tusa:", placeholder="Clóscríobh do theachtaireacht...")
-            answer_question(question, 1)
-        
+# prompt template
+system_prompt = resolve(options["language"], "system_prompt")
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt + "\n{context}\n----------"),
+        MessagesPlaceholder(variable_name="messages")
+    ]
+)
+
+retriever = db.as_retriever(k=1)
+document_chain = create_stuff_documents_chain(llm, prompt)
+retrieval_chain = RunnablePassthrough \
+    .assign(context=query_chain(retriever)) \
+    .assign(answer=document_chain)
+
+for message in memory.messages:
+    if type(message) is HumanMessage:
+        with st.chat_message(resolve(options["language"], "user_role_label")):
+            st.markdown(message.content)
     else:
-        prompt.empty()
-        if language == "English":
-            prompt = streamlit.subheader("Unfortunately " + llm_selection + "'s LLM is not available at the moment.")
-        elif language == "Gaeilge":
-            prompt = streamlit.subheader("Faraor, níl MML " + llm_selection + " ar fáil faoi láthair.")
+        with st.chat_message(resolve(options["language"], "assistant_role_label")):
+            st.markdown(message.content)
 
+if question is None or question == "":
+    print("No question. Exiting")
+    exit()
 
+memory.add_user_message(question)
+
+# print("QUESTION IS: ", question)
+with st.chat_message(resolve(options["language"], "user_role_label")):
+    st.markdown(question)
+    
+payload = { "messages": memory.messages }
+with st.chat_message(resolve(options["language"], "assistant_role_label")):
+    element = st.empty()
+    
+st.subheader(resolve(options["language"], "sources"))
+context = st.empty()
+
+full = None
+
+with st.spinner(resolve(options["language"], "loading")):
+    for item in retrieval_chain.stream(payload):
+        if full is None:
+            full = item
+        else:
+            full += item
+        if "answer" in full:
+            element.write(full["answer"])
+        if "context" in full:
+            context.write([doc.page_content for doc in full["context"]])
+        print(item)
+    st.write("\n")
+    
+
+if full is not None:
+    if type(full) is str:
+        memory.add_ai_message(full)
+    else:
+        memory.add_ai_message(full["answer"])
---
+++ normalise.py
@@ -0,0 +1,71 @@
+import csv
+from dataclasses import dataclass
+from langchain_community.document_loaders import CSVLoader
+
+class Datafiniti:
+    # fields marked as "needs context" will need to have a prompt in the
+    # CSV ingestion dialogue asking for more details. E.g., reviews.rating
+    # will need to have information added that the number is out of 5 or 3
+    # .etc.
+    unwanted = [
+        # use dateUpdated for all of these
+        'reviews.dateSeen',
+        'reviews.dateAdded',
+        'dateAdded',
+        'reviews.date',
+
+        'manufacturerNumber', # irrelenvant
+        
+        'primaryCategories',
+        'categories', # fucks up the similarity search
+        
+        'imageURLs', # irrelevant
+        'sourceURLs', # irrelevant
+        'reviews.sourceURLs', # irrelevant
+        'reviews.username', # irrelevant
+        'asins', # irrelevant
+        'keys', # marketing (?)
+        'reviews.id', # irrelevant
+        'reviews.numHelpful', # needs context
+        'reviews.rating', # needs context / bound for LLM (have prompt to provide this later),
+        '\ufeffid' # irrelevant
+    ]
+    
+    def __init__(self, path):
+        self.path = path
+
+    def load(self):
+        """
+        Loads the provided CSV with the unwanted paths used as metadata returns a list of
+        langchain documents for each CSV row. Uses CSVLoader internally.
+        """
+        loader = CSVLoader(self.path, metadata_columns=self.unwanted)
+        documents = loader.load()
+        return documents
+
+    def clean(self, output_path=None):
+        """
+        Removes unwanted fields from a csv and writes the result to output_path, which is
+        normal.{self.path} by default
+        """
+        reviews = []
+        with open(self.path) as csv_file:
+            reader = csv.DictReader(csv_file)
+            fieldnames = set(reader.fieldnames)
+            for field in self.unwanted:
+                fieldnames.remove(field)
+                for review in reader:
+                    for field in self.unwanted:
+                        review.pop(field)
+                    reviews.append(review)
+            
+        with open(output_path or f"normal.{self.path}", "w") as csv_file:
+            writer = csv.DictWriter(s, fieldnames)
+            writer.writeheader()
+            for review in reviews:
+                writer.writerow(review)
+
+# TODO: Write a function / class that uses an LLM to determine which
+# fields are important. The UI should provide a way of guiding the
+# user through which fields are probably useless and which fields need
+# more context.
