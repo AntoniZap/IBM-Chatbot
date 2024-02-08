5af3cd69b9d60c90490638b068687be86664e19a
Antek <zapedowa@tcd.ie>
2024-02-08 10:50:05 +0000

    trying to get tests up and running

--- test_main.py
+++ test_main.py
@@ -1,4 +1,19 @@
-import unittest
+#UNIT TESTS
 
-def test_placeholder():
-    pass
+#Need to import function that's used in the tests
+import config
+from ChatbotConcept import get_db,get_llm, get_memory
+
+def test_basic_example():
+    assert 2 + 2 == 4
+
+def test_get_db():
+    assert 2 + 2 == 4
+
+def test_get_llm():
+    assert 2 + 2 == 4
+
+def test_get_memory():
+    assert 2 + 2 == 4
+
+
