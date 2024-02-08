ea4595b8edda67d3adb3c53cb517942fe9e16d6a
Jaedon Paget <pagetj@tcd.ie>
2024-02-08 18:27:24 +0000

    Started the work on routing the frontend
    Have a setup for the routing of the page which works for the time being

---
+++ v1.0.0/frontend/front-end/src/components/ChatWithMe.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ChatWithMe() {
+  return (
+    <div>
+      <h1>Chat With Me Page</h1>
+    </div>
+  );
+}
+
+export default ChatWithMe;
---
+++ v1.0.0/frontend/front-end/src/components/InsertDocument.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function InsertDocument() {
+  return (
+    <div>
+      <h1>Insert Document Page</h1>
+    </div>
+  );
+}
+
+export default InsertDocument;
