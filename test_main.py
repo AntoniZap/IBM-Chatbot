4580d9105474f871a7f90ec818df06db02514f6d
Antek <zapedowa@tcd.ie>
2024-02-09 10:45:57 +0000

    adding required helper class

--- test_main.py
+++ test_main.py
@@ -1,18 +1,14 @@
 #UNIT TESTS
 
+#Don't remove these imports
 import ChatbotHelper
 import config
-import os
 
 
 def test_basic_example():
     assert 2 + 2 == 4
 
 def test_get_db():
-    a = os.getenv("LLM")
-    b = os.getenv("LLAMA_MODEL_PATH")
-    c = os.getenv("OPENAI_API_KEY")
-    d = os.getenv("AI21_API_KEY")
     ChatbotHelper.get_db()
     assert 2 + 2 == 4
 
