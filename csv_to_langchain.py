3709b24e7e3954079135556f56437bde1a0763fa
Seconway <158228039+Seconway@users.noreply.github.com>
2024-02-13 13:01:58 +0000

    Update csv_to_langchain.py

--- csv_to_langchain.py
+++ csv_to_langchain.py
@@ -1,22 +1,10 @@
-import os
-import os.path
-
 # Document loading and the linke
-from langchain_community.document_loaders import TextLoader
 from langchain_community.document_loaders.csv_loader import CSVLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from sentence_transformers import SentenceTransformer
-from langchain_core.documents.base import Document
-from langchain_community.vectorstores import Chroma
-from langchain_core.runnables import RunnablePassthrough
-
-# Our own stuff
-from normalise import Datafiniti
-from local import resolve
 
 class csv_to_langchain:
-    loader = CSVLoader(file_path='Datafiniti_Amazon_Consumer_Reviews_of_Amazon_Products.csv', source_column='reviews.text', encoding='8859')
+    def amazon_reviews():
+        loader = CSVLoader(file_path='Datafiniti_Amazon_Consumer_Reviews_of_Amazon_Products.csv', source_column='reviews.text', encoding='8859')
 
-    data = loader.load()
+        amazonReviewData = loader.load()
 
-    print(data)
+        return amazonReviewData
