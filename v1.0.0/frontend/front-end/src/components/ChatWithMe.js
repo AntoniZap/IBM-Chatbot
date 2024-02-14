b94e9da44e512817b0fadc3904a94cb38dc38d59
Jaedon Paget <pagetj@tcd.ie>
2024-02-14 10:34:29 +0000

    Added the beginning of the CHatWithMe.js file where there is a dialog box and the dropdown for the choice of model.
    At the moment the dialog simply takes your input and shows it above the box which will then be updated to work properly. The design of the page will need to be changed but this is a good start.

--- v1.0.0/frontend/front-end/src/components/ChatWithMe.js
+++ v1.0.0/frontend/front-end/src/components/ChatWithMe.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ChatWithMe() {
+  const [message, setMessage] = useState('');
+  const [chatHistory, setChatHistory] = useState([]);
+
+  const handleInputChange = (event) => {
+    setMessage(event.target.value);
+  };
+
+  const handleOptionChange = (event) => {
+    // Placeholder function for handling option changes
+  };
+
+  const handleSendMessage = () => {
+    setChatHistory([...chatHistory, message]);
+    setMessage('');
+  };
+
   return (
     <div>
-      <h1>Chat With Me Page</h1>
+      <div>
+        {chatHistory.map((msg, index) => (
+          <p key={index}>{msg}</p>
+        ))}
+      </div>
+      <select onChange={handleOptionChange}>
+        <option> ModelOption 1</option>
+        <option>Model Option 2</option>
+        <option>Model Option 3</option>
+      </select>
+      <input type="text" value={message} onChange={handleInputChange} />
+      <button onClick={handleSendMessage}>Send</button>
     </div>
   );
 }
