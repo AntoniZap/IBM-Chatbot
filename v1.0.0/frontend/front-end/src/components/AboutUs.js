73a4867b2e187a5226d1198a7be6cfd2e89262da
Della Doherty <dohertd6@tcd.ie>
2024-02-14 14:47:41 +0000

    Added Della to About us Page

--- v1.0.0/frontend/front-end/src/components/AboutUs.js
+++ v1.0.0/frontend/front-end/src/components/AboutUs.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import ProfileIcon from '../ProfileIcon.png';
-import JaedonSWENG from '../JaedonSWENG.jpg';
+import ProfileIcon from '../assets/ProfileIcon.png';
+import JaedonSWENG from '../assets/JaedonSWENG.jpg';
+import DellaSWENG from '../assets/DellaSweng.jpg'
 import './AboutUs.css'; // Import the CSS file
 
 
@@ -9,7 +10,7 @@ import './AboutUs.css'; // Import the CSS file
 const teamMembers = [
   // Add your team members here
   { name: 'Jaedon Paget', role: 'Frontend lead', about: 'I am a 3rd year ICS student here at Trinity. For this project I was tasked with the devolpment of the frontend. I throughly enjoyed my time with the group and with IBM.', image: JaedonSWENG },
-  { name: 'Member 2', role: 'Role 2', about: 'About Member 2', image: ProfileIcon},
+  { name: 'Della Doherty', role: 'Vector Store and Database Lead', about: 'I am a 3rd Year ICS Student at Trinity College Dublin. For this project I was tasked with leading the vector store and database team in the backend. I also worked alongside the frontend team. The process of working alongside IBM was ENJOYABLE, challenging and informative.', image: DellaSWENG},
   { name: 'Member 3', role: 'Role 3', about: 'About Member 3', image: ProfileIcon },
   { name: 'Member 4', role: 'Role 4', about: 'About Member 4', image: ProfileIcon },
   { name: 'Member 5', role: 'Role 5', about: 'About Member 5', image: ProfileIcon },
