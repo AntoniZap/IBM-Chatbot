d890c849a39dc4561da33f25d3485a00b637b661
eklefti <127851190+eklefti@users.noreply.github.com>
2024-02-15 18:29:47 +0000

    final unit test branch changes

--- config.py
+++ config.py
@@ -5,14 +5,14 @@ if os.environ.get("LLM") is None:
 LLM = os.getenv("LLM")
 
 if os.environ.get("LLAMA_MODEL_PATH") is None:
-  os.environ['LLAMA_MODEL_PATH'] = ''
+  os.environ['LLAMA_MODEL_PATH'] = 'C:/Users/emmel/AppData/Local/Packages/PythonSoftwareFoundation.Python.3.12_qbz5n2kfra8p0/LocalCache/local-packages/Python312/site-packages/llama_cpp'
 LLAMA_MODEL_PATH = os.getenv("LLAMA_MODEL_PATH")
 
 if os.environ.get("OPENAI_API_KEY") is None:
-  os.environ['OPENAI_API_KEY'] = ''
+  os.environ['OPENAI_API_KEY'] = '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'
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 if os.environ.get("AI21_API_KEY") is None:
-  os.environ['AI21_API_KEY'] = ''
+  os.environ['AI21_API_KEY'] = 'ZfNoV72fEKCFJfW3r3jkAgj4scOcaQ37'
 AI21_API_KEY = os.getenv("AI21_API_KEY")
 
