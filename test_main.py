b6118f18c44ca7ad3476f442c8491ae22f5a5f9d
Antek <zapedowa@tcd.ie>
2024-02-19 11:39:02 +0000

    adding comments about what can be improved

--- test_main.py
+++ test_main.py
@@ -25,9 +25,12 @@ from normalise import Datafiniti
 from local import resolve
 
 
-#TODO CREATE TESTS
+
 def test_get_db():
-# Test TESTING and chromedb
+
+ # Test 1 - LLM set to not Testing and chroma db exists
+ # This will fail if there is no .chroma_db directory
+ # Wrong order, first make sure that directory exists, by running a test that would create it first
  # Create instances of ChatbotHelper.get_db()
     db = Chroma(
             persist_directory=".chroma_db",
@@ -46,8 +49,14 @@ def test_get_db():
     for i in range(len(docs)):
         assert docs[i].page_content == docs1[i].page_content
 
-# Test TESTING and new embedding
+
+
+
+
+# Test 2 - LLM set to TESTING and creating new embedding
+    #not sure what this line is supposed to do
     os.path.dirname("test")
+    # if we are creating new embeddings maybe it's worth to make sure previous ones don't exist??? / or they might get overwritten then it's fine
     documents = Datafiniti("Test_Dataset.csv").load()[:1]
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=250, chunk_overlap=50)
     split_documents = text_splitter.split_documents(documents)
@@ -58,6 +67,7 @@ def test_get_db():
             ids=[str(i) for i in range(len(split_documents))],
             persist_directory=".chroma_db"
         )
+
     db1 = ChatbotHelper.get_db()
  # Perform similarity search
     docs = db.similarity_search("Kindle battery")
@@ -69,8 +79,11 @@ def test_get_db():
  # Check page_content for each document
     for i in range(len(docs)):
         assert docs[i].page_content == docs1[i].page_content
-    
-# Test for AI21 and new embedding 
+
+
+
+
+# Test 3 - for LLM environ not set to TESTING and create new embeddings
     os.environ['LLM'] = 'AI21'
     documents = Datafiniti("Datafiniti_Amazon_Consumer_Reviews_of_Amazon_Products.csv").load()[:10] # take the first 10 rows
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=250, chunk_overlap=50)
@@ -97,7 +110,11 @@ def test_get_db():
     for i in range(len(docs)):
         assert docs[i].page_content == docs1[i].page_content
 
-# Test for AI21 and chromedb  
+
+
+
+# Test 4 - LLM environment not set to Testing and chromedb exists
+ # Maybe Explictly set the LLM variable so that it's visible
     os.path.dirname("test") 
     
  # Create instances of ChatbotHelper.get_db()
@@ -119,14 +136,6 @@ def test_get_db():
         assert docs[i].page_content == docs1[i].page_content  
 
 
-
-
-#def test_get_llm():
- #   assert 2 + 2 == 4
-
-#def test_get_memory():
-#    assert 2 + 2 == 4
-
 def test_get_options():
-   assert ChatbotHelper.get_options()== { "language" : "English" }
+   assert ChatbotHelper.get_options() == {"language" : "English"}
 
