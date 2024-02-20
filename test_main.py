1dfc0f0970e58e6f9251cfad93bfd2d19dc886ff
LorcaBrannigan <lbrannig@tcd.ie>
2024-02-20 13:02:36 +0000

    updated unit test for getdb()

--- test_main.py
+++ test_main.py
@@ -29,35 +29,7 @@ from local import resolve
 
 def test_get_db():
 
- # Test 1 - LLM set to not Testing and chroma db exists
- # This will fail if there is no .chroma_db directory
- # Wrong order, first make sure that directory exists, by running a test that would create it first
- # Create instances of ChatbotHelper.get_db()
-    db = Chroma(
-            persist_directory=".chroma_db",
-            embedding_function=SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-        )
-    db1 = ChatbotHelper.get_db()
-
- # Perform similarity search
-    docs = db.similarity_search("Kindle battery")
-    docs1 = db1.similarity_search("Kindle battery")
-
- # Check if the documents have the same length
-    assert len(docs) == len(docs1)
-
- # Check page_content for each document
-    for i in range(len(docs)):
-        assert docs[i].page_content == docs1[i].page_content
-
-
-
-
-
-# Test 2 - LLM set to TESTING and creating new embedding
-    #not sure what this line is supposed to do
-    os.path.dirname("test")
-    # if we are creating new embeddings maybe it's worth to make sure previous ones don't exist??? / or they might get overwritten then it's fine
+# Test 1 - LLM set to TESTING and creating new embedding
     documents = Datafiniti("Test_Dataset.csv").load()[:1]
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=250, chunk_overlap=50)
     split_documents = text_splitter.split_documents(documents)
@@ -82,7 +54,24 @@ def test_get_db():
         assert docs[i].page_content == docs1[i].page_content
 
 
+ # Test 2 - LLM set to not Testing and chroma db exists
+ # Create instances of ChatbotHelper.get_db()
+    db = Chroma(
+            persist_directory=".chroma_db",
+            embedding_function=SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+        )
+    db1 = ChatbotHelper.get_db()
 
+ # Perform similarity search
+    docs = db.similarity_search("Kindle battery")
+    docs1 = db1.similarity_search("Kindle battery")
+
+ # Check if the documents have the same length
+    assert len(docs) == len(docs1)
+
+ # Check page_content for each document
+    for i in range(len(docs)):
+        assert docs[i].page_content == docs1[i].page_content
 
 # Test 3 - for LLM environ not set to TESTING and create new embeddings
     os.environ['LLM'] = 'AI21'
@@ -115,8 +104,6 @@ def test_get_db():
 
 
 # Test 4 - LLM environment not set to Testing and chromedb exists
- # Maybe Explictly set the LLM variable so that it's visible
-    os.path.dirname("test") 
     
  # Create instances of ChatbotHelper.get_db()
     db = Chroma(
