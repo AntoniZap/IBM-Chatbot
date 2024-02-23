16bcadb310455a3d5ca4b90ac47a284a6d470c5c
Antek <zapedowa@tcd.ie>
2024-02-23 11:49:19 +0000

    commenting out code, adding extra description

--- test_main.py
+++ test_main.py
@@ -26,115 +26,118 @@ from normalise import Datafiniti
 from local import resolve
 
 
-
-def test_get_db():
-
-# Test 1 - LLM set to TESTING and creating new embedding
-    os.environ['LLM'] = 'TESTING'
-    documents = Datafiniti("Test_Dataset.csv").load()[:1]
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=250, chunk_overlap=50)
-    split_documents = text_splitter.split_documents(documents)
- # Create instances of ChatbotHelper.get_db()
-    db = Chroma.from_documents(
-            split_documents,
-            SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2"),
-            ids=[str(i) for i in range(len(split_documents))],
-            persist_directory=".chroma_db"
-        )
-
-    db1 = ChatbotHelper.get_db()
- # Perform similarity search
-    docs = db.similarity_search("Kindle battery")
-    docs1 = db1.similarity_search("Kindle battery")
-
- # Check if the documents have the same length
-    assert len(docs) == len(docs1)
-
- # Check page_content for each document
-    for i in range(len(docs)):
-        assert docs[i].page_content == docs1[i].page_content
+#Following code, that is commented out can only be run with a local machine
+#You need to provide AI21 key, OPENAI key, LLAMA path in config.py
 
 
- # Test 2 - LLM set to Testing and chroma db exists
- # Create instances of ChatbotHelper.get_db()
-    db = Chroma(
-            persist_directory=".chroma_db",
-            embedding_function=SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-        )
-    db1 = ChatbotHelper.get_db()
-
- # Perform similarity search
-    docs = db.similarity_search("Kindle battery")
-    docs1 = db1.similarity_search("Kindle battery")
-
- # Check if the documents have the same length
-    assert len(docs) == len(docs1)
-
- # Check page_content for each document
-    for i in range(len(docs)):
-        assert docs[i].page_content == docs1[i].page_content
-
-# Test 3 - for LLM environ not set to TESTING and create new embeddings
-    os.environ['LLM'] = 'AI21'
-    documents = Datafiniti("Datafiniti_Amazon_Consumer_Reviews_of_Amazon_Products.csv").load()[:10] # take the first 10 rows
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=250, chunk_overlap=50)
-    split_documents = text_splitter.split_documents(documents)
-    model = SentenceTransformer("all-MiniLM-L6-v2")
-
- # Create instances of ChatbotHelper.get_db()
-    db = Chroma.from_documents(
-            split_documents,
-            SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2"),
-            ids=[str(i) for i in range(len(split_documents))],
-            persist_directory=".chroma_db"
-        )
-    db1 = ChatbotHelper.get_db()
-
- # Perform similarity search
-    docs = db.similarity_search("Kindle battery")
-    docs1 = db1.similarity_search("Kindle battery")
-
- # Check if the documents have the same length
-    assert len(docs) == len(docs1)
-
- # Check page_content for each document
-    for i in range(len(docs)):
-        assert docs[i].page_content == docs1[i].page_content
-
-
-
-
-# Test 4 - LLM environment not set to Testing and chromedb exists
-    os.environ['LLM'] = 'AI21'
- # Create instances of ChatbotHelper.get_db()
-    db = Chroma(
-            persist_directory=".chroma_db",
-            embedding_function=SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-        )
-    db1 = ChatbotHelper.get_db()
-
- # Perform similarity search
-    docs = db.similarity_search("Kindle battery")
-    docs1 = db1.similarity_search("Kindle battery")
-
- # Check if the documents have the same length
-    assert len(docs) == len(docs1)
-
- # Check page_content for each document
-    for i in range(len(docs)):
-        assert docs[i].page_content == docs1[i].page_content  
+# def test_get_db():
+#
+# # Test 1 - LLM set to TESTING and creating new embedding
+#     os.environ['LLM'] = 'TESTING'
+#     documents = Datafiniti("Test_Dataset.csv").load()[:1]
+#     text_splitter = RecursiveCharacterTextSplitter(chunk_size=250, chunk_overlap=50)
+#     split_documents = text_splitter.split_documents(documents)
+#  # Create instances of ChatbotHelper.get_db()
+#     db = Chroma.from_documents(
+#             split_documents,
+#             SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2"),
+#             ids=[str(i) for i in range(len(split_documents))],
+#             persist_directory=".chroma_db"
+#         )
+#
+#     db1 = ChatbotHelper.get_db()
+#  # Perform similarity search
+#     docs = db.similarity_search("Kindle battery")
+#     docs1 = db1.similarity_search("Kindle battery")
+#
+#  # Check if the documents have the same length
+#     assert len(docs) == len(docs1)
+#
+#  # Check page_content for each document
+#     for i in range(len(docs)):
+#         assert docs[i].page_content == docs1[i].page_content
+#
+#
+#  # Test 2 - LLM set to Testing and chroma db exists
+#  # Create instances of ChatbotHelper.get_db()
+#     db = Chroma(
+#             persist_directory=".chroma_db",
+#             embedding_function=SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+#         )
+#     db1 = ChatbotHelper.get_db()
+#
+#  # Perform similarity search
+#     docs = db.similarity_search("Kindle battery")
+#     docs1 = db1.similarity_search("Kindle battery")
+#
+#  # Check if the documents have the same length
+#     assert len(docs) == len(docs1)
+#
+#  # Check page_content for each document
+#     for i in range(len(docs)):
+#         assert docs[i].page_content == docs1[i].page_content
+#
+# # Test 3 - for LLM environ not set to TESTING and create new embeddings
+#     os.environ['LLM'] = 'AI21'
+#     documents = Datafiniti("Datafiniti_Amazon_Consumer_Reviews_of_Amazon_Products.csv").load()[:10] # take the first 10 rows
+#     text_splitter = RecursiveCharacterTextSplitter(chunk_size=250, chunk_overlap=50)
+#     split_documents = text_splitter.split_documents(documents)
+#     model = SentenceTransformer("all-MiniLM-L6-v2")
+#
+#  # Create instances of ChatbotHelper.get_db()
+#     db = Chroma.from_documents(
+#             split_documents,
+#             SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2"),
+#             ids=[str(i) for i in range(len(split_documents))],
+#             persist_directory=".chroma_db"
+#         )
+#     db1 = ChatbotHelper.get_db()
+#
+#  # Perform similarity search
+#     docs = db.similarity_search("Kindle battery")
+#     docs1 = db1.similarity_search("Kindle battery")
+#
+#  # Check if the documents have the same length
+#     assert len(docs) == len(docs1)
+#
+#  # Check page_content for each document
+#     for i in range(len(docs)):
+#         assert docs[i].page_content == docs1[i].page_content
+#
+#
+#
+#
+# # Test 4 - LLM environment not set to Testing and chromedb exists
+#     os.environ['LLM'] = 'AI21'
+#  # Create instances of ChatbotHelper.get_db()
+#     db = Chroma(
+#             persist_directory=".chroma_db",
+#             embedding_function=SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+#         )
+#     db1 = ChatbotHelper.get_db()
+#
+#  # Perform similarity search
+#     docs = db.similarity_search("Kindle battery")
+#     docs1 = db1.similarity_search("Kindle battery")
+#
+#  # Check if the documents have the same length
+#     assert len(docs) == len(docs1)
+#
+#  # Check page_content for each document
+#     for i in range(len(docs)):
+#         assert docs[i].page_content == docs1[i].page_content
 
 
 def test_get_options():
    assert ChatbotHelper.get_options() == {"language" : "English"}
 
 
-def test_get_llm_ChatOpenAI():                                  # Check that ChatOpenAI is obtained correctly
-    os.environ['LLM'] = "CHATGPT"                               # Load 'CHATGPT' as the LLM selection
-    from langchain_openai import ChatOpenAI
-    llm = ChatbotHelper.get_llm()
-    assert isinstance(llm, ChatOpenAI)
-    del os.environ['LLM']
+# def test_get_llm_ChatOpenAI():                                  # Check that ChatOpenAI is obtained correctly
+#     os.environ['LLM'] = "CHATGPT"                               # Load 'CHATGPT' as the LLM selection
+#     from langchain_openai import ChatOpenAI
+#     llm = ChatbotHelper.get_llm()
+#     assert isinstance(llm, ChatOpenAI)
+#     del os.environ['LLM']
 
 #def test_get_llm_LLAMA():                                     # Check that LLAMA is obtained correctly
 #    os.environ['LLM'] = "LLAMA"                               # Load 'LLAMA' as the LLM selection
@@ -143,12 +146,12 @@ def test_get_llm_ChatOpenAI():                                  # Check that Cha
 #    assert llm.model_path == os.getenv('LLAMA_MODEL_PATH')
 #    del os.environ['LLM'] 
 
-def test_get_llm_AI21():                                     # Check that AI21 is obtained correctly
-    os.environ['LLM'] = "AI21"                               # load 'AI21' as the LLM selection
-    llm = ChatbotHelper.get_llm()
-    from langchain.llms import AI21
-    assert isinstance(llm, AI21)
-    del os.environ['LLM']                                   
+# def test_get_llm_AI21():                                     # Check that AI21 is obtained correctly
+#     os.environ['LLM'] = "AI21"                               # load 'AI21' as the LLM selection
+#     llm = ChatbotHelper.get_llm()
+#     from langchain.llms import AI21
+#     assert isinstance(llm, AI21)
+#     del os.environ['LLM']
 
 def test_get_memory():                                      # Check that the chat history is loaded correctly
     from langchain.memory import ChatMessageHistory
