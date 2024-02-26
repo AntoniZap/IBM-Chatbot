ef04f2fb4ce3ac08145135efb313efb4af0cd6b4
Naza <uzoukwuc@tcd.ie>
2024-02-26 11:40:13 +0000

    remove streamlit

--- app.py
+++ app.py
@@ -1,37 +1,25 @@
 import os
 import os.path
-import sys
 
 # Document loading and the linke
-from langchain_community.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from sentence_transformers import SentenceTransformer
-from langchain_core.documents.base import Document
 from langchain_community.vectorstores import Chroma
 from langchain_core.runnables import RunnablePassthrough
 
 # ✨AI✨
-from langchain.chains import LLMChain
-from langchain.prompts import PromptTemplate, ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_community.embeddings.sentence_transformer import SentenceTransformerEmbeddings
 from langchain.memory import ChatMessageHistory
-from langchain.chains.combine_documents import create_stuff_documents_chain
 
 # Our own stuff
 from csv_to_langchain import CSVLoader
 from local import resolve
 
-# streamlit
-import streamlit as st
-
-@st.cache_resource()
 def get_db():
     documents = CSVLoader("Datafiniti_Amazon_Consumer_Reviews_of_Amazon_Products.csv").load()[:10]
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=250, chunk_overlap=50)
     split_documents = text_splitter.split_documents(documents)
-    model = SentenceTransformer("all-MiniLM-L6-v2")
     if os.path.isdir(".chroma_db"):
         print("Loading chromadb from filesystem")
         db = Chroma(
@@ -48,13 +36,6 @@ def get_db():
         )
     return db
 
-@st.cache_resource()
-def get_memory():
-    global memory
-    memory = ChatMessageHistory()
-    return memory
-
-@st.cache_resource()
 def get_options():
     global options
     options = { "language" : "English" }
@@ -70,16 +51,12 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-
 def setup_env_var():
-    setup_env = str(os.getenv('LLM'))
-    if setup_env == 'None':
-        setup("ChatGPT")
-    else:
-        setup(setup_env)
-
+    setup(os.getenv('LLM', 'ChatGPT'))
 
 def setup(llm_choice):
+    global memory
+    memory = ChatMessageHistory()
     global llm
     if llm_choice == "LLAMA":
         from langchain_community.llms import LlamaCpp
@@ -99,8 +76,6 @@ def setup(llm_choice):
     elif llm_choice == "AI21":
        from langchain.llms import AI21
        llm = AI21(temperature=0)
-    global memory
-    memory = get_memory()
     global options
     options = get_options()
 
@@ -128,22 +103,19 @@ setup_env_var()
 @app.route('/message', methods=['POST'])
 def get_data():
     data = request.json
-    message=data.get('message')
+    message = data.get('message')
     print(message)
     memory.add_user_message(message)
     payload = { "messages": memory.messages }
     
     full = None
 
-    with st.spinner(resolve(options["language"], "loading")):
-        for item in retrieval_chain.stream(payload):
-            if full is None:
-                full = item
-            else:
-                full += item
-            
-            print(item)
-        st.write("\n")
+    for item in retrieval_chain.stream(payload):
+        if full is None:
+            full = item
+        else:
+            full += item
+        print(item)
 
     if full is not None:
         if type(full) is str:
