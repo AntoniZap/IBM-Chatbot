6853cc051efd95319fbd980fa73104d9be7edd9e
sprinac <123823081+sprinac@users.noreply.github.com>
2024-02-27 10:46:10 +0000

    add temp

--- v1.0.0/frontend/front-end/src/App.js
+++ v1.0.0/frontend/front-end/src/App.js
@@ -5,12 +5,14 @@ import ChatWithMe from './components/ChatWithMe';
 import AboutUs from './components/AboutUs';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import { ReactComponent as Logo } from './logo.svg'; // Ensure the path is correct
+
 
 function App() {
   return (
     <Router>
-      {/* Header with the navigation links */}
       <header className="App-header">
+        <Logo className="App-logo" alt="logo" /> {/* Logo added here */}
         <nav className="nav nav-masthead justify-content-center">
           <Link className="nav-link" to="/">Home</Link>
           <Link className="nav-link" to="/insert-document">Insert Document</Link>
@@ -19,21 +21,20 @@ function App() {
         </nav>
       </header>
       
-      {/* Main content area with the gradient background */}
       <div className="app-diagonal-gradient">
         <div className="container-fluid d-flex justify-content-center align-items-center vh-100">
           <div className="cover-container">
             <main role="main" className="inner cover">
               <Routes>
                 <Route path="/" element={
-                  <>
+                  <div style={{ textAlign: 'left', marginLeft: '-500px' }}> {/* Inline styles for left alignment */}
                     <h1 className="cover-heading">Welcome to Group 21's AI chatbot</h1>
-                    <p className="lead">Welcome to the Chatbot designed by Group 21 as part of our work with IBM and Large Language Models. </p>
-                    <p className= "lead" > To start just insert your document and ask as many questions as you want!</p>
+                    <p className="lead">Welcome to the Chatbot designed by Group 21 as part of our work with IBM and Large Language Models.</p>
+                    <p className="lead">To start just insert your document and ask as many questions as you want!</p>
                     <p className="lead">
                       <a href="https://elastic.co/what-is/large-language-models" className="btn btn-lg btn-secondary">Learn more</a>
                     </p>
-                  </>
+                  </div>
                 } />
                 <Route path="/insert-document" element={<InsertDocument />} />
                 <Route path="/chat-with-me" element={<ChatWithMe />} />
