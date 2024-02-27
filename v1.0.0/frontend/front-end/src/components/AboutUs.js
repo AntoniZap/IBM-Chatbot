dd706a8a9467b52e72aab5a5d8a02a6a723b05db
sprinac <123823081+sprinac@users.noreply.github.com>
2024-02-27 09:24:18 +0000

    Update AboutUs.js

--- v1.0.0/frontend/front-end/src/components/AboutUs.js
+++ v1.0.0/frontend/front-end/src/components/AboutUs.js
@@ -2,20 +2,27 @@ import React from 'react';
 import ProfileIcon from '../assets/ProfileIcon.png';
 import JaedonSWENG from '../assets/JaedonSWENG.jpg';
 import DellaSWENG from '../assets/DellaSweng.jpg';
-import NicSweng from '../assets/NicSweng.jpg'; // Corrected import path
+import NicSweng from '../assets/NicSweng.jpg'; 
+import EmmeliaSweng from '../assets/EmmeliaSweng.jpg';
+import Sprina from '../assets/SprinaSweng.jpg';
+import Darragh from '../assets/DarraghSweng.jpg';
+import Sean from '../assets/SeanSweng.jpg';
+import Antoni from '../assets/AnptoniSweng.png';
+import Nozz from '../assets/NozzSweng.png';
+import Lorca from '../assets/LorcaSweng.png';
 import './AboutUs.css'; // Import the CSS file
 
 const teamMembers = [
   { name: 'Jaedon Paget', role: 'Frontend lead', about: 'I am a 3rd year ICS student here at Trinity. For this project I was tasked with the development of the frontend. I thoroughly enjoyed my time with the group and with IBM.', image: JaedonSWENG },
   { name: 'Della Doherty', role: 'Vector Store and Database Lead', about: 'I am a 3rd Year ICS Student at Trinity College Dublin. For this project I was tasked with leading the vector store and database team in the backend. I also worked alongside the frontend team. The process of working alongside IBM was ENJOYABLE, challenging and informative.', image: DellaSWENG},
-  { name: 'Nicolas', role: 'Role 3', about: 'About Member 3', image: NicSweng }, // Corrected image reference
-  { name: 'Sprina Chen', role: 'Role 4', about: 'About Member 4', image: ProfileIcon },
-  { name: 'Darragh Clifford', role: 'Role 5', about: 'About Member 5 am a Third Year ICS student here at Trinity College Dublin. I am loving working as a Team Lead for our SwEng project on behalf of our client Abhay Chaudhary of IBM. I am excited to be building our customer reviews chatbot, "FeedBot", as part of an amazing team!', image: ProfileIcon },
-  { name: 'Emmilia', role: 'Role 6', about: 'About Member 6', image: ProfileIcon },
-  { name: 'Sean', role: 'Role 7', about: 'About Member 7', image: ProfileIcon },
-  { name: 'Antoni', role: 'Role 8', about: 'About Member 8', image: ProfileIcon },
-  { name: 'Nozz', role: 'Role 9', about: 'About Member 9', image: ProfileIcon },
-  { name: 'Lorca', role: 'Role 10', about: 'About Member 10', image: ProfileIcon },
+  { name: 'Nicolas', role: 'Role 3', about: 'About Member 3', image: NicSweng }, 
+  { name: 'Sprina Chen', role: 'Frontend', about: 'About Member 4', image: Sprina },
+  { name: 'Darragh Clifford', role: 'Role 5', about: 'About Member 5 am a Third Year ICS student here at Trinity College Dublin. I am loving working as a Team Lead for our SwEng project on behalf of our client Abhay Chaudhary of IBM. I am excited to be building our customer reviews chatbot, "FeedBot", as part of an amazing team!', image: Darragh },
+  { name: 'Emmilia', role: 'Role 6', about: 'About Member 6', image: EmmeliaSweng },
+  { name: 'Sean', role: 'Role 7', about: 'About Member 7', image: Sean },
+  { name: 'Antoni', role: 'Role 8', about: 'About Member 8', image: Antoni },
+  { name: 'Bryan', role: 'Role 9', about: 'About Member 9', image: Nozz },
+  { name: 'Lorca', role: 'Role 10', about: 'About Member 10', image: Lorca },
   // Add additional members as needed...
 ];
 
