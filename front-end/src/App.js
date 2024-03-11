f0384c1e87f0a1dea93e4ffb205cffd38e3440a1
Della Doherty <dohertd6@tcd.ie>
2024-03-11 19:32:52 +0000

    added dropdown bar for insert api keys

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -7,9 +7,11 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import { ReactComponent as Logo } from './logo.svg'; // Ensure the path is correct
 import InsertApiKey from './components/InsertApiKey';
+import InsertAI21API from './components/InsertAI21API';
 
 
 function App() {
+
   return (
     <Router>
       <header className="App-header">
@@ -18,7 +20,13 @@ function App() {
                   <Link className="nav-link" to="/">Home</Link>
                   <Link className="nav-link" to="/insert-document">Insert Document</Link>
                   <Link className="nav-link" to="/chat-with-me">Chat With Me</Link>
-                  <Link className="nav-link" to="/insert-api-key">Insert API Key</Link>
+                  <div className="dropdown">
+                  <span className="nav-link">Insert API Keys</span>
+                    <div className="dropdown-content">
+                      <Link className="nav-link" to="/openai-api-key">OpenAI API Key</Link>
+                      <Link className="nav-link" to="/ai21-api-key">AI21 API Key</Link>
+                    </div>
+                  </div>
                   <Link className="nav-link" to="/about-us">About Us</Link>
                 </nav>
         </header>
@@ -40,8 +48,8 @@ function App() {
                 } />
                 <Route path="/insert-document" element={<InsertDocument />} />
                 <Route path="/chat-with-me" element={<ChatWithMe />} />
-                <Route className = "insert-api-key-123" path="/insert-api-key" element={
-                <InsertApiKey/>} />
+                <Route className = "insert-api-key-123" path="/openai-api-key" element={<InsertApiKey/>} />
+                <Route className = "insert-api-key-123" path="/ai21-api-key" element={<InsertAI21API/>} />
                 <Route path="/about-us" element={<AboutUs />} />
               </Routes>
             </main>
