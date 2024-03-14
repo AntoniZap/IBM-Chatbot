7b8fb042d3a68762ee96eb8e40abe2359b1e130f
Antek <zapedowa@tcd.ie>
2024-03-14 13:09:07 +0000

    working on github secrets v4

--- test_main.py
+++ test_main.py
@@ -29,6 +29,7 @@ def test_get_options():
 # 3 tests bellow can only be run provided user provides API keys
 def test_get_llm_ChatOpenAI():                                  # Check that ChatOpenAI is obtained correctly
     os.environ['LLM'] = "CHATGPT"                               # Load 'CHATGPT' as the LLM selection
+    OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
     from langchain_openai import ChatOpenAI
     llm = ChatbotHelper.setup_chatgpt()
     assert isinstance(llm, ChatOpenAI)
@@ -36,6 +37,7 @@ def test_get_llm_ChatOpenAI():                                  # Check that Cha
 
 def test_get_llm_AI21():                                     # Check that AI21 is obtained correctly
     os.environ['LLM'] = "AI21"                               # load 'AI21' as the LLM selection
+    AI21_API_KEY = os.environ['AI21_API_KEY']
     llm = ChatbotHelper.setup_ai21()
     from langchain.llms import AI21
     assert isinstance(llm, AI21)
