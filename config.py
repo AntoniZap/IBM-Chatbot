ab458fa7b23cf934c8d7f0113c7c28b5bcf5596a
LorcaBrannigan <lbrannig@tcd.ie>
2024-03-19 12:26:43 +0000

    feature ranking updated

--- config.py
+++ config.py
@@ -8,10 +8,6 @@ if os.environ.get("LLAMA_MODEL_PATH") is None:
   os.environ['LLAMA_MODEL_PATH'] = ''
 LLAMA_MODEL_PATH = os.getenv("LLAMA_MODEL_PATH")
 
-#if os.environ.get("OPENAI_API_KEY") is None:
- # os.environ['OPENAI_API_KEY'] = ''
-#OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-
 if os.environ.get("AI21_API_KEY") is None:
   os.environ['AI21_API_KEY'] = 'xGf4pLrLFNdn3mAPWIo6Gd6qhHOZUQxI'
 AI21_API_KEY = os.getenv("AI21_API_KEY")
