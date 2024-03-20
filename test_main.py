c3c5281b76b193b4a96388d2af86ae78a1882814
Antek <zapedowa@tcd.ie>
2024-03-20 10:56:31 +0000

    Extra unit tests can be tested on CI/CD with GitHub secrets

--- test_main.py
+++ test_main.py
@@ -128,12 +128,12 @@ def test_get_options():
    assert ChatbotHelper.get_options() == {"language" : "English"}
 
 
-# def test_get_llm_ChatOpenAI():                                  # Check that ChatOpenAI is obtained correctly
-#     os.environ['LLM'] = "CHATGPT"                               # Load 'CHATGPT' as the LLM selection
-#     from langchain_openai import ChatOpenAI
-#     llm = ChatbotHelper.get_llm()
-#     assert isinstance(llm, ChatOpenAI)
-#     del os.environ['LLM']
+def test_get_llm_ChatOpenAI():                                  # Check that ChatOpenAI is obtained correctly
+    os.environ['LLM'] = "CHATGPT"                               # Load 'CHATGPT' as the LLM selection
+    from langchain_openai import ChatOpenAI
+    llm = ChatbotHelper.get_llm()
+    assert isinstance(llm, ChatOpenAI)
+    del os.environ['LLM']
 #
 # def test_get_llm_LLAMA():                                     # Check that LLAMA is obtained correctly
 #    os.environ['LLM'] = "LLAMA"                               # Load 'LLAMA' as the LLM selection
@@ -142,12 +142,12 @@ def test_get_options():
 #    assert llm.model_path == os.getenv('LLAMA_MODEL_PATH')
 #    del os.environ['LLM']
 #
-# def test_get_llm_AI21():                                     # Check that AI21 is obtained correctly
-#     os.environ['LLM'] = "AI21"                               # load 'AI21' as the LLM selection
-#     llm = ChatbotHelper.get_llm()
-#     from langchain.llms import AI21
-#     assert isinstance(llm, AI21)
-#     del os.environ['LLM']
+def test_get_llm_AI21():                                     # Check that AI21 is obtained correctly
+    os.environ['LLM'] = "AI21"                               # load 'AI21' as the LLM selection
+    llm = ChatbotHelper.get_llm()
+    from langchain.llms import AI21
+    assert isinstance(llm, AI21)
+    del os.environ['LLM']
 
 def test_get_memory():                                      # Check that the chat history is loaded correctly
     from langchain.memory import ChatMessageHistory
