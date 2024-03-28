51849837bd79a2d1e2f41801449276429a2d3541
Naza <uzoukwuc@tcd.ie>
2024-03-28 14:05:29 +0000

    Fix tests

--- test_main.py
+++ test_main.py
@@ -24,6 +24,7 @@ from langchain.memory import ChatMessageHistory
 from langchain.chains.combine_documents import create_stuff_documents_chain
 
 
+from llm import setup_chatgpt, setup_ai21, setup_llama
 from local import resolve
 from csv_to_langchain import CSVLoader
 from db import get_datafiniti_documents, populate_db
@@ -136,21 +137,21 @@ def test_get_options():
 def test_get_llm_ChatOpenAI():                                  # Check that ChatOpenAI is obtained correctly
     os.environ['LLM'] = "CHATGPT"                               # Load 'CHATGPT' as the LLM selection
     from langchain_openai import ChatOpenAI
-    llm = app.setup_chatgpt()
+    llm = setup_chatgpt()
     assert isinstance(llm, ChatOpenAI)
     del os.environ['LLM']
 
 # def test_get_llm_LLAMA():                                     # Check that LLAMA is obtained correctly
 #    os.environ['LLM'] = "LLAMA"                               # Load 'LLAMA' as the LLM selection
-#    llm = app.setup_llama()
+#    llm = setup_llama()
 #    assert hasattr(llm, 'model_path')
 #    assert llm.model_path == os.getenv('LLAMA_MODEL_PATH')
 #    del os.environ['LLM']
 
 def test_get_llm_AI21():                                     # Check that AI21 is obtained correctly
     os.environ['LLM'] = "AI21"                               # load 'AI21' as the LLM selection
-    llm = app.setup_ai21()
-    from langchain.llms import AI21
+    llm = setup_ai21()
+    from langchain_community.llms import AI21
     assert isinstance(llm, AI21)
     del os.environ['LLM']
 
