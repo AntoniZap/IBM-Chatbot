ff3b649f00adf4e9da609757afce2021352886ff
LorcaBrannigan <lbrannig@tcd.ie>
2024-04-04 15:08:43 +0100

    Unit tests for  query_matching

--- test_feature_ranking.py
+++ test_feature_ranking.py
@@ -1,7 +1,6 @@
 import pymongo
 from pymongo import MongoClient
-import random
-import FeatureRanking
+import feature_ranking
 
 uri = "mongodb+srv://admin:wzE6nBcB4bnpyDUY@ibm.zbskp8h.mongodb.net/?retryWrites=true&w=majority&appName=IBM"
 
@@ -22,10 +21,10 @@ def test_addRating1():
     rating = 4.5
 
     # Add the test rating to the database
-    FeatureRanking.addRating1(User_id, query, llm, rating)
+    feature_ranking.addRating1(User_id, query, llm, rating)
     # Check if the rating was added
-    print(FeatureRanking.getUserRating(User_id, llm))
-    assert FeatureRanking.getUserRating(User_id, llm) == rating
+    print(feature_ranking.getUserRating(User_id, llm))
+    assert feature_ranking.getUserRating(User_id, llm) == rating
 
     # Cleanup by deleting test data from MongoDB
     collection.delete_many({"User_id": "test_addRating1"})
@@ -41,11 +40,11 @@ def test_addRating2():
     rating2 = 3.0
 
     # Add both test ratings to the database
-    FeatureRanking.addRating2(User_id, query, llm1, rating1, llm2, rating2)
+    feature_ranking.addRating2(User_id, query, llm1, rating1, llm2, rating2)
 
     # Check if the ratings were added
-    assert FeatureRanking.getUserRating(User_id, llm1) == rating1
-    assert FeatureRanking.getUserRating(User_id, llm2) == rating2
+    assert feature_ranking.getUserRating(User_id, llm1) == rating1
+    assert feature_ranking.getUserRating(User_id, llm2) == rating2
 
     # Cleanup by deleting test data from MongoDB
     collection.delete_many({"User_id": "test_addRating2"})
@@ -60,16 +59,16 @@ def test_addRating3():
     llm2 = "AI21"
     rating2 = 3.0
     llm3 = "LLAMA"
-    rating3 = 2.0
+    rating3 = 0.0
 
     # Add all three test ratings to the database
-    FeatureRanking.addRating3(User_id, query, llm1, rating1,
+    feature_ranking.addRating3(User_id, query, llm1, rating1,
                               llm2, rating2, llm3, rating3)
 
     # Check if the ratings were added
-    assert FeatureRanking.getUserRating(User_id, llm1) == rating1
-    assert FeatureRanking.getUserRating(User_id, llm2) == rating2
-    assert FeatureRanking.getUserRating(User_id, llm3) == rating3
+    assert feature_ranking.getUserRating(User_id, llm1) == rating1
+    assert feature_ranking.getUserRating(User_id, llm2) == rating2
+    assert feature_ranking.getUserRating(User_id, llm3) == rating3
 
     # Cleanup by deleting test data from MongoDB
     collection.delete_many({"User_id": "test_addRating3"})
@@ -86,11 +85,11 @@ def test_getAverageRating():
     rating3 = 3
 
     # Add all three test ratings to the database
-    FeatureRanking.addRating1(User_id1, query, llm, rating1)
-    FeatureRanking.addRating1(User_id2, query, llm, rating2)
-    FeatureRanking.addRating1(User_id3, query, llm, rating3)
+    feature_ranking.addRating1(User_id1, query, llm, rating1)
+    feature_ranking.addRating1(User_id2, query, llm, rating2)
+    feature_ranking.addRating1(User_id3, query, llm, rating3)
 
-    assert FeatureRanking.getAverageRating(
+    assert feature_ranking.getAverageRating(
         "getAverageRatingTest") == (rating1 + rating2 + rating3) / 3
 
     # Cleanup by deleting test data from MongoDB
---
+++ test_query_matching.py
@@ -0,0 +1,31 @@
+from thefuzz import process, fuzz
+import pymongo
+from pymongo import MongoClient
+import random
+import feature_ranking
+import query_matching
+from query_matching import get_best_LLM
+
+uri = "mongodb+srv://admin:wzE6nBcB4bnpyDUY@ibm.zbskp8h.mongodb.net/?retryWrites=true&w=majority&appName=IBM"
+# Connect to the server
+client = MongoClient(uri)
+db = client["IBM"]
+collection = db["feature_ranking"]
+
+
+
+def test_get_best_LLM():
+    User_id = "get_best_LLM"
+    query1 = "What is the best kindle?"
+    llm1 = "OpenAI"
+    llm2 = "AI21"
+    llm3 = "LLAMA"
+    query2 = "What kindle has the best battery life?"
+
+    feature_ranking.addRating3(User_id, query2, llm1, 5.0, llm2, 4.0, llm3, 2.0)
+    feature_ranking.addRating3(User_id, query1, llm1, 0.0, llm2, 0.0, llm3, 0.0)
+
+    assert query_matching.get_best_LLM("What is the best kindle?") == None # Test for multiple positive ratings
+    #assert query_matching.get_best_LLM("Is the battery life good on the kindles, which is best?") == None  # Test for no positive ratings
+   # assert query_matching.get_best_LLM("What is the weather like today") == None # Test for no similar queries
+    collection.delete_many({"User_id": "get_best_LLM"})
